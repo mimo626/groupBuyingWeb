4de9afea14a3784047984d2007d3bf87940fb662
kangminju <yessuk5499@gmail.com>
2026-05-08 17:39:35 +0900

    feat: Repository 어노테이션 추가

--- src/main/java/com/example/groupbuyingweb/repository/GroupBuyingRepository.java
+++ src/main/java/com/example/groupbuyingweb/repository/GroupBuyingRepository.java
@@ -2,6 +2,8 @@ package com.example.groupbuyingweb.repository;
 
 import com.example.groupbuyingweb.domain.entity.GroupBuying;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupBuyingRepository extends JpaRepository<GroupBuying, Long> {
 }
