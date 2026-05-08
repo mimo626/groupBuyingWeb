62a710ac0fb7678d539fc76d38a964298278e7e7
kangminju <yessuk5499@gmail.com>
2026-05-08 17:41:03 +0900

    feat: Member의 생성날짜 CreationTimestamp로 변경

--- src/main/java/com/example/groupbuyingweb/domain/entity/Member.java
+++ src/main/java/com/example/groupbuyingweb/domain/entity/Member.java
@@ -2,7 +2,7 @@ package com.example.groupbuyingweb.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.springframework.data.annotation.CreatedDate;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -40,8 +40,8 @@ public class Member {
     @Column(nullable = false)
     private Integer acornExp;
 
-    @CreatedDate
-    @Column(updatable = false)
+    @CreationTimestamp
+    @Column(updatable = false, columnDefinition = "TIMESTAMP")
     private LocalDateTime createdAt;
 
     @PrePersist
