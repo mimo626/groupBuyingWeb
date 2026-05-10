400969040de961c4fde96b4048077ca3b54c4c27
kangminju <yessuk5499@gmail.com>
2026-05-10 18:22:29 +0900

    feat: GroupBuyingRepository에 함수 추가

--- src/main/java/com/example/groupbuyingweb/repository/GroupBuyingRepository.java
+++ src/main/java/com/example/groupbuyingweb/repository/GroupBuyingRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GroupBuyingRepository extends JpaRepository<GroupBuying, Long> {
+    Integer findTargetQuantityById(long groupBuyingId);
 }
