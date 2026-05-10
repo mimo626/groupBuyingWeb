2d811fa75a677521b5685aa657ab89d84d7f3d6b
kangminju <yessuk5499@gmail.com>
2026-05-10 18:21:33 +0900

    feat: GlobalViewExceptionHandler 에러 핸들링

--- src/main/java/com/example/groupbuyingweb/core/error/GlobalViewExceptionHandler.java
+++ src/main/java/com/example/groupbuyingweb/core/error/GlobalViewExceptionHandler.java
@@ -1,4 +1,23 @@
 package com.example.groupbuyingweb.core.error;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalViewExceptionHandler {
+
+    // IllegalStateException
+    @ExceptionHandler(IllegalStateException.class)
+    public String handleIllegalStateException(IllegalStateException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/alertAndBack"; // 사용자에게 알림창 띄우고 뒤로가기 하는 뷰(html)로 이동
+    }
+
+    // IllegalArgumentException
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handleIllegalArgumentException(IllegalArgumentException ex, Model model) {
+        model.addAttribute("errorMessage", ex.getMessage());
+        return "error/404"; // 404 페이지로 이동
+    }
 }
