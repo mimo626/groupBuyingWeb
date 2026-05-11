92b6a62c0aadc5b70a97a850f84e2cbd42a7c3bf
HyunseokLEE <unducklee@gmail.com>
2026-05-11 22:10:27 +0900

    feat: 로그인 기능 추가

--- src/main/java/com/example/groupbuyingweb/repository/MemberRepository.java
+++ src/main/java/com/example/groupbuyingweb/repository/MemberRepository.java
@@ -3,7 +3,10 @@ package com.example.groupbuyingweb.repository;
 import com.example.groupbuyingweb.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, String> {
     boolean existsByLoginId(String loginId);
     boolean existsByNickname(String nickname);
+    Optional<Member> findByLoginId(String loginId);
 }
