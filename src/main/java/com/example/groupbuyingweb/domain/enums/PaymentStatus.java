68e430da6884d58d3f03084e45af580a70f6d606
kangminju <yessuk5499@gmail.com>
2026-05-14 09:04:42 +0900

    feat: 정산 상태 이름 수정

--- src/main/java/com/example/groupbuyingweb/domain/enums/PaymentStatus.java
+++ src/main/java/com/example/groupbuyingweb/domain/enums/PaymentStatus.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PaymentStatus {
-    Incomplete("정산 대기"),
+    Incomplete("정산 미완료"),
     Complete("정산 완료");
     private final String description;
     }
