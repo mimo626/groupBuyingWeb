140a2156d8528c065ec4d1866d3614f623cb960b
HyunseokLEE <unducklee@gmail.com>
2026-05-18 00:14:50 +0900

    refactor: HTML 내부 JavaScript URL 상수화 리팩토링 & login페이지 url 수정

--- src/main/java/com/example/groupbuyingweb/controller/AuthPageController.java
+++ src/main/java/com/example/groupbuyingweb/controller/AuthPageController.java
@@ -13,7 +13,7 @@ public class AuthPageController {
 
     @GetMapping("/auth/login") // 추가: 로그인 화면을 컨트롤러 매핑 주소로 열기 위해 추가
     public String loginPage() {
-        return "login";
+        return "forward:/auth/login.html"; // 수정: login.html이 static에 있으므로 정적 파일로 forward
     }
 
     @GetMapping("/auth/signup")
