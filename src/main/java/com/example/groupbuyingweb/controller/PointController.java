8f7d97c19f1b74d86a75e4d9caf4afeac074c305
minsu919 <minsur0919@gmail.com>
2026-05-18 15:09:14 +0900

    feat : 충전, 공구참여현황 페이지 /url 수정 MyPageController.java -> MemberController.java 리소스 재정의 PointController.java 는 다음 PR에 삭제하고 메서드는 GroupBuyingController로 옮기겠습니다.

--- src/main/java/com/example/groupbuyingweb/controller/PointController.java
+++ src/main/java/com/example/groupbuyingweb/controller/PointController.java
@@ -20,27 +20,6 @@ public class PointController {
     @Autowired
     private LoginSessionManager loginSessionManager;
 
-//    @GetMapping("/refund")
-//    public ApiResponse<?> refundPoint(int 사용자id ,int 공구참여id, int 포인트){
-//
-//    }
-//
-    //@PostMapping("/api/point/charge")
-    @PatchMapping("/api/members/me/point") //멤버리소스/마이페이지/포인트리소스 업데이트
-    @ResponseBody
-    public ApiResponse<?> chargePoint(
-            // 성공 : 200 OK
-            // 메시지 : 포인트 충전되었습니다.
-            // 실패 : 500 서버 내부 오류
-            // 실패 : 401 로그인 요청
-            @RequestBody GroupBuyingParticipationRequest.Charge request,HttpSession session){
-        String memberId = loginSessionManager.requireLoginUserId(session);
-        double charge = request.point();
-        GroupBuyingParticipationResponse.UserResult dto = pointService.chargePoint(memberId, charge);
-        return ApiResponse.success(dto);
-    }
-
-    //@PostMapping("/api/point/settle")
     @PostMapping("/api/group-buyings/{groupBuyingId}/settlements/me") // 복수의 공구리소스 /중 하나의/(추상적)정산 리소스/마이페이지
     @ResponseBody
     public ApiResponse<?> sendPointToOrganizer(
@@ -49,7 +28,6 @@ public class PointController {
             // dto : 정산된 공구 id, 전체 정산 여부 True/False
             // 실패 : 500 서버 내부 오류
             // 실패 : 401 로그인 오류
-            @RequestBody GroupBuyingParticipationRequest.Send request,
             @PathVariable Long groupBuyingId,
             HttpSession session){
         String memberId = loginSessionManager.requireLoginUserId(session);
