8164dd0eff8321c3354eaf590f16484af287f46e
kangminju <yessuk5499@gmail.com>
2026-05-25 03:08:07 +0900

    feat: 안 쓰는 import 제거

--- src/main/java/com/example/groupbuyingweb/config/security/SecurityConfig.java
+++ src/main/java/com/example/groupbuyingweb/config/security/SecurityConfig.java
@@ -1,7 +1,4 @@
 package com.example.groupbuyingweb.config.security;
-import com.example.groupbuyingweb.repository.mysql.GroupBuyingRepository;
-import com.example.groupbuyingweb.service.GroupBuyingService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
